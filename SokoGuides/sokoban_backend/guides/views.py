7b4f211d111fe4ecb59c1516f2469424bb15f685
Hesed <hesedguwn@gmail.com>
2025-02-21 16:19:45 -0500

    new branch test added stuff too

--- SokoGuides/sokoban_backend/guides/views.py
+++ SokoGuides/sokoban_backend/guides/views.py
@@ -1,17 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from rest_framework import viewsets
 from .models import Game, Guide, User
 from .serializers import GameSerializer, GuideSerializer, UserSerializer
+from .forms import GuideForm
 
 # New homepage view (renders HTML)
 def home(request):
     return render(request, 'home.html',{})  # Assumes "home.html" exists in your templates folder
 
 def signin(request):
-    return render(request, 'sign-in.html',{})  # Assumes "home.html" exists in your templates folder
+    return render(request, 'sign-in.html',{})  
 
 def signup(request):
-    return render(request, 'sign-up.html',{})  # Assumes "home.html" exists in your templates folder
+    return render(request, 'sign-up.html',{})  
+
+def create_guide(request):
+    if request.method == 'POST':
+        form = GuideForm(request.POST)
+        if form.is_valid():
+            guide = form.save(commit=False)
+            guide.user = request.user  # Assign the logged-in user
+            guide.save()
+            return redirect('home')  # Redirect after successful creation
+    else:
+        form = GuideForm()
+    
+    return render(request, 'guides/create-guide.html', {'form': form})
 
 # API Views
 class UserViewSet(viewsets.ModelViewSet):
