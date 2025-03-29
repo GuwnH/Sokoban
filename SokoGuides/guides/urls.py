366fbe48d5a4b12fc4895f256d299d1c09138605
Hesed <hesedguwn@gmail.com>
2025-03-29 16:02:19 -0400

    made stuff

--- SokoGuides/guides/urls.py
+++ SokoGuides/guides/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import home, signin, signup, create_guide, UserViewSet, GameViewSet, GuideViewSet
+from .views import home, signin, signup, create_guide, guide_detail, profile_detail, search_guides, UserViewSet, GameViewSet, GuideViewSet
 
 # API router for DRF
 router = DefaultRouter()
@@ -14,4 +14,7 @@ urlpatterns = [
     path('sign-in', signin, name='sign-in'),
     path('sign-up', signup, name='sign-up'),
     path('guides/create/', create_guide, name='create_guide'),
+    path('guides/<int:guide_id>/', guide_detail, name='guide_detail'),
+    path('profiles/<int:user_id>/', profile_detail, name='profile_detail'),
+    path('search/', search_guides, name='search_guides'),
 ]
--- SokoGuides/guides/views.py
+++ SokoGuides/guides/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from rest_framework import viewsets
-from .models import Game, Guide, User
+from .models import Game, Guide, User, Image
 from .serializers import GameSerializer, GuideSerializer, UserSerializer
 from .forms import GuideForm
 
@@ -26,6 +26,61 @@ def create_guide(request):
         form = GuideForm()
     
     return render(request, 'make_guides.html', {'form': form})
+
+def guide_detail(request, guide_id):
+    # Fetch the guide by its ID
+    guide = get_object_or_404(Guide, guide_id=guide_id)
+    
+    # Fetch related images for the guide
+    images = Image.objects.filter(guide=guide)
+    
+    # Pass the guide and images to the template
+    return render(request, 'view_guide.html', {
+        'guide': guide,
+        'images': images,
+    })
+
+def profile_detail(request, user_id):
+    # Fetch the guide by its ID
+    user = get_object_or_404(User, user_id=user_id)
+    
+    # Fetch related guides associated with the user
+    guides = Guide.objects.filter(user=user_id)
+    
+    # Pass the guide and images to the template
+    return render(request, 'profile.html', {
+        'guides': guides,
+        'user': user,
+    })
+
+def search_guides(request):
+    # Get search parameters from the request
+    game_name = request.GET.get('game_name', '')
+    guide_level = request.GET.get('guide_level', '')
+    user_name = request.GET.get('user_name', '')
+
+    # Start with all guides
+    guides = Guide.objects.all()
+
+    # Filter by game name (case-insensitive)
+    if game_name:
+        guides = guides.filter(game__game_name__icontains=game_name)
+
+    # Filter by guide level
+    if guide_level:
+        guides = guides.filter(guide_level=guide_level)
+
+    # Filter by user name (case-insensitive)
+    if user_name:
+        guides = guides.filter(user__user_name__icontains=user_name)
+
+    # Pass the filtered guides to the template
+    return render(request, 'guide_search.html', {
+        'guides': guides,
+        'game_name': game_name,
+        'guide_level': guide_level,
+        'user_name': user_name,
+    })
 
 # API Views
 class UserViewSet(viewsets.ModelViewSet):
