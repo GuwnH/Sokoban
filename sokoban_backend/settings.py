c759450e6a7b42bb270c439041dd73677bd46579
Eric Appiah Adjei <adjei.net@Erics-MacBook-Pro-12349.local>
2025-04-14 22:41:30 -0400

    fixing database_url issues

--- sokoban_backend/settings.py
+++ sokoban_backend/settings.py
@@ -72,13 +72,19 @@ WSGI_APPLICATION = 'sokoban_backend.wsgi.application'
 
 
 # Database configuration
-DATABASES = {
-    'default': dj_database_url.config(
-        default=config('DATABASE_URL'),
-        conn_max_age=600,
-        ssl_require=not DEBUG
-    )
-}
+DATABASE_URL = config('DATABASE_URL', default=None)
+
+if DATABASE_URL:
+    DATABASES = {
+        'default': dj_database_url.parse(DATABASE_URL, conn_max_age=600, ssl_require=not DEBUG)
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 AUTH_PASSWORD_VALIDATORS = [
