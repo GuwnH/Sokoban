1796bc640623aa87287af25b48588c213c78a0e3
pozolin <yystudent6@gmail.com>
2025-04-22 16:42:54 -0400

    changed settings.py

--- sokoban_backend/settings.py
+++ sokoban_backend/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 from pathlib import Path
 import os
 from decouple import config
+DEBUG = config('DEBUG', cast=bool)
 import dj_database_url
 
 BASE_DIR = Path(__file__).resolve().parent.parent
