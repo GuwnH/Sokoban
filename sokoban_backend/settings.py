d47d5a72a5f6f6d4c0e3a4c1833b1bc648c3e681
Hesed <hesedguwn@gmail.com>
2025-04-26 12:29:55 -0400

    trying to get it on vm

--- sokoban_backend/settings.py
+++ sokoban_backend/settings.py
@@ -82,14 +82,22 @@ if DATABASE_URL:
 else:
     DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'sokoguides2',
-        'USER': 'root',
-        'PASSWORD': 'Bobsloblobtob00!',
-        'HOST': '127.0.0.1',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# else:
+#     DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'sokoguides2',
+#         'USER': 'root',
+#         'PASSWORD': 'Bobsloblobtob00!',
+#         'HOST': '127.0.0.1',
+#         'PORT': '3306',
+#     }
+# }
 
 AUTH_PASSWORD_VALIDATORS = [
     {
